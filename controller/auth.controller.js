f01dcd010d18171f0a80d8f40747df6d5416a8ca
Amhjadkhan <amjadpogramer@gmail.com>
2024-08-17 13:51:26 +0400

    this is cooke parse auth solved and signup done commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,4 +1,53 @@
-export const signup = (req, res) =>{}
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from 'jsonwebtoken';
+
+
+export const signup = async (req, res, next) => {
+    const { username, email, password, confirmPassword, gender } = req.body
+  
+    let validUser
+  
+    validUser = await User.findOne({ email })
+  
+    if (validUser) {
+      return next(errorHandler(400, "User already exists"))
+    }
+  
+    if (password !== confirmPassword) {
+      return next(errorHandler(400, "Password don't match"))
+    }
+  
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+  
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+  
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    })
+  
+    try {
+      // generate jwt token
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+  
+      await newUser.save()
+  
+      res.cookie("access_token", token, { httpOnly: true }).status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+  
 
 export const login = (req, res) =>{}
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
@@ -12,16 +13,21 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(express.json())
+app.use(cookieParser())
+
+
 
 
 app.get('/', (req, res) => {
   res.send('this is chatapplication server running')
 })
 
-// import routes 
+//import routes 
 import authRoutes from './routes/auth.routes.js'
 
-app.use("./api/auth", authRoutes)
+app.use("/api/auth", authRoutes)
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
